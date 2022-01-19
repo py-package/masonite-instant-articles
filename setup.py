236114bb550e0da83556a82ee4b96f67184f433b
Yubaraj Shrestha <companion.krish@gmail.com>
2022-01-19 23:22:26 +0545

    Updated Version

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.0.4',
 
     description='Instant Article for Masonite',
     long_description=long_description,
     long_description_content_type="text/markdown",
 
     # The project's main homepage.
-    url="https://github.com/yubarajshrestha/masonite-instant-article",
+    url="https://github.com/yubarajshrestha/masonite-instant-articles",
 
     # Author details
     author='Yubaraj Shrestha',
